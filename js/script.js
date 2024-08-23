79d4ec703a1fc02c8769f90225cb947656695082
VeRyM <verym2004@gmail.com>
2024-08-23 21:35:54 +1100

    add header and section-promo

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// const selectArrow = document.querySelectorAll(".form-search__select");
+
+// selectArrow.forEach(function(elem) {
+//     elem.addEventListener("click", ()=> {
+//         elem.classList.toggle("change");
+//     });
+// });
